e12593b31cb71d8d0a18445e0c3cb321099402a5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-01-07 19:49:02 +1100

    isBlank function()
    - Closes https://github.com/mP1/walkingkooka-tree-expression-function-boolean/issues/27
    - isBlank

---
+++ src/main/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionBooleanIsBlank.java
@@ -0,0 +1,67 @@
+
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.expression.function;
+
+import walkingkooka.spreadsheet.function.SpreadsheetExpressionFunctionContext;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameter;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterName;
+
+import java.util.List;
+
+// https://support.google.com/docs/answer/3093290?hl=en&ref_topic=3105471
+// Checks whether the referenced cell is empty. whether a formula is in the referenced cell.
+// Only a cell-reference that loads no SpreadsheetCell will return true.
+final class SpreadsheetExpressionFunctionBooleanIsBlank extends SpreadsheetExpressionFunctionBoolean {
+
+    /**
+     * Singleton
+     */
+    final static SpreadsheetExpressionFunctionBooleanIsBlank INSTANCE = new SpreadsheetExpressionFunctionBooleanIsBlank();
+
+    private SpreadsheetExpressionFunctionBooleanIsBlank() {
+        super("isBlank");
+    }
+
+    @Override
+    public Boolean apply(final List<Object> parameters,
+                         final SpreadsheetExpressionFunctionContext context) {
+        this.checkOnlyRequiredParameters(parameters);
+
+        final Object maybeReference = REFERENCE.getOrFail(parameters, 0);
+        boolean blank = false;
+
+        if (maybeReference instanceof SpreadsheetCellReference) {
+            blank = !context.loadCell((SpreadsheetCellReference) maybeReference)
+                    .isPresent();
+        }
+
+        return blank;
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters() {
+        return PARAMETERS;
+    }
+
+    final static ExpressionFunctionParameter<Object> REFERENCE = ExpressionFunctionParameterName.with("reference")
+            .setType(Object.class);
+
+    private final static List<ExpressionFunctionParameter<?>> PARAMETERS = ExpressionFunctionParameter.list(REFERENCE);
+}
---
+++ src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionBooleanIsBlankTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.expression.function;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetExpressionFunctionBooleanIsBlankTest extends SpreadsheetExpressionFunctionBooleanTestCase<SpreadsheetExpressionFunctionBooleanIsBlank> {
+
+    @Test
+    public void testTwoParametersFails() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> this.apply2(
+                        "1a", "2b"
+                )
+        );
+    }
+
+    @Test
+    public void testNull() {
+        this.isBlankAndCheck(
+                null,
+                false
+        );
+    }
+
+    @Test
+    public void testZero() {
+        this.isBlankAndCheck(
+                ExpressionNumberKind.DOUBLE.create(0),
+                false
+        );
+    }
+
+    @Test
+    public void testEmptyString() {
+        this.isBlankAndCheck(
+                "",
+                false
+        );
+    }
+
+    @Test
+    public void testExistingCellReference() {
+        this.isBlankAndCheck(
+                LOAD_CELL_REFERENCE,
+                false
+        );
+    }
+
+    @Test
+    public void testMissingCellReference() {
+        this.isBlankAndCheck(
+                REFERENCE,
+                true
+        );
+    }
+
+    @Test
+    public void testRangeTopLeftMissingCellReference() {
+        this.isBlankAndCheck(
+                REFERENCE.spreadsheetCellRange(
+                        REFERENCE.add(1, 1)
+                ),
+                false
+        );
+    }
+
+    @Test
+    public void testRange() {
+        this.isBlankAndCheck(
+                LOAD_CELL_REFERENCE.spreadsheetCellRange(
+                        LOAD_CELL_REFERENCE.add(2, 3)
+                ),
+                false
+        );
+    }
+
+    private void isBlankAndCheck(final Object value,
+                                 final boolean expected) {
+        this.applyAndCheck2(
+                Lists.of(
+                        value
+                ),
+                expected
+        );
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(this.createBiFunction(), "isBlank");
+    }
+
+    @Override
+    public SpreadsheetExpressionFunctionBooleanIsBlank createBiFunction() {
+        return SpreadsheetExpressionFunctionBooleanIsBlank.INSTANCE;
+    }
+
+    @Override
+    public Class<SpreadsheetExpressionFunctionBooleanIsBlank> type() {
+        return SpreadsheetExpressionFunctionBooleanIsBlank.class;
+    }
+}
