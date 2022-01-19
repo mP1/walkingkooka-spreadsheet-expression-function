53731eb44bff9cea1d1571f1bcfc0aefb638e7e6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-01-19 21:47:12 +1100

    na() functions
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-expression-function/issues/55
    - na()

---
+++ src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionNaTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.expression.function;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.SpreadsheetError;
+import walkingkooka.spreadsheet.SpreadsheetErrorKind;
+
+public final class SpreadsheetExpressionFunctionNaTest extends SpreadsheetExpressionFunctionTestCase<SpreadsheetExpressionFunctionNa, SpreadsheetError> {
+
+    @Test
+    public void testIsErr() {
+        this.checkEquals(
+                false,
+                SpreadsheetExpressionFunctions.isErr()
+                        .apply(
+                                Lists.of(
+                                        this.apply2()
+                                ),
+                                this.createContext()
+                        )
+        );
+    }
+
+    @Test
+    public void testIsNa() {
+        this.checkEquals(
+                true,
+                SpreadsheetExpressionFunctions.isNa()
+                        .apply(
+                                Lists.of(
+                                        this.apply2()
+                                ),
+                                this.createContext()
+                        )
+        );
+    }
+
+    @Test
+    public void testApply() {
+        this.applyAndCheck2(
+                Lists.empty(),
+                SpreadsheetErrorKind.NA.setMessage("NA()")
+        );
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                this.createBiFunction(),
+                "na"
+        );
+    }
+
+    @Override
+    public SpreadsheetExpressionFunctionNa createBiFunction() {
+        return SpreadsheetExpressionFunctionNa.INSTANCE;
+    }
+
+    @Override
+    public Class<SpreadsheetExpressionFunctionNa> type() {
+        return SpreadsheetExpressionFunctionNa.class;
+    }
+
+    @Override
+    public String typeNamePrefix() {
+        return SpreadsheetExpressionFunction.class.getSimpleName();
+    }
+}
