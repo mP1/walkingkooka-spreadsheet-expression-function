21ee493458dc5dc0c6cd2bf2ab8ea73b220173b8
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-09 07:20:09 +1000

    SpreadsheetExpressionFunctionStringFormulaTextTest.testCellMissingFormulaFail empty test removed

--- src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionStringFormulaTextTest.java
+++ src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionStringFormulaTextTest.java
@@ -48,11 +48,6 @@ public final class SpreadsheetExpressionFunctionStringFormulaTextTest extends Sp
         );
     }
 
-    @Test
-    public void testCellMissingFormulaFail() {
-
-    }
-
     @Override
     public SpreadsheetExpressionFunctionStringFormulaText createBiFunction() {
         return SpreadsheetExpressionFunctionStringFormulaText.INSTANCE;
