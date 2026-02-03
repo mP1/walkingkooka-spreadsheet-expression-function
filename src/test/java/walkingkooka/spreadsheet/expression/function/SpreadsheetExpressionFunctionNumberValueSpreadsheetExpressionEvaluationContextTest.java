cde0aa1cdc493bb466d77e53633bcc6738ef2798
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-02-03 17:40:16 +1100

    https://github.com/mP1/walkingkooka-environment/pull/311 EnvironmentContext.timeOffset/setTimeOffset
    - https://github.com/mP1/walkingkooka-environment/pull/311
    - EnvironmentContext.timeOffset/setTimeOffset
    
    - https://github.com/mP1/walkingkooka-storage-expression-function/pull/83
    - StorageExpressionFunctionReadText: convert value using StoragePath

--- src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionNumberValueSpreadsheetExpressionEvaluationContextTest.java
+++ src/test/java/walkingkooka/spreadsheet/expression/function/SpreadsheetExpressionFunctionNumberValueSpreadsheetExpressionEvaluationContextTest.java
@@ -175,6 +175,11 @@ public final class SpreadsheetExpressionFunctionNumberValueSpreadsheetExpression
     }
 
     @Override
+    public void testSetTimeOffsetWithDifferentAndWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public void testSetUserWithDifferentAndWatcher() {
         throw new UnsupportedOperationException();
     }
